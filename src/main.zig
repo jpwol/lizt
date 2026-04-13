90cfd53802602de101166d19d61bfaa8049bddec
jpwol <jpwolfe1981@gmail.com>
2026-04-13 03:34:42 -0400

    refactor: moved print logic to files.zig

--- src/main.zig
+++ src/main.zig
@@ -24,31 +24,11 @@ pub fn main(init: std.process.Init) !void {
     };
     
     const filestat = file.init(path, s, init.io, init.gpa);
-    var list = filestat.buildDirList() catch |err| {
+
+    filestat.printlist(stdout) catch |err| {
         try stderr.print("{s}: '{s}': {s}\n", .{argv[0], path, @errorName(err)});
         return;
     };
-    var max_width: usize = 0;
-    for (list.items) |i| {
-        const width = std.fmt.count("{d}", .{i.size});
-        if (width > max_width) max_width = width;
-    }
-
-    for (list.items) |i| {
-        const name = std.c.getpwuid(i.uid);
-        const group = std.c.getgrgid(i.gid);
-
-        try stdout.print("\x1b[36m{[perm]s} \x1b[32m{[user]s} \x1b[33m{[group]s} \x1b[34m{[size]d:>[width]} \x1b[35m{[name]s:<5}\n", .{
-            .perm = i.perm,
-            .user = name.?.name.?, 
-            .group = group.?.name.?, 
-            .size = i.size, 
-            .width = max_width, 
-            .name = i.name
-        }); 
-    }
-
-    filestat.deinit(&list);
     
     try stdout.flush();
     try stderr.flush();
