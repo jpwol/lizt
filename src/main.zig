d284bc88ef5890de7d04bcc41b7a794c444a33d3
jpwol <jpwolfe1981@gmail.com>
2026-04-13 02:47:22 -0400

    temp(main): testing dynamic formatting

--- src/main.zig
+++ src/main.zig
@@ -28,9 +28,23 @@ pub fn main(init: std.process.Init) !void {
         try stderr.print("{s}: '{s}': {s}\n", .{argv[0], path, @errorName(err)});
         return;
     };
+    var max_width: usize = 0;
+    for (list.items) |i| {
+        const width = std.fmt.count("{d}", .{i.size});
+        if (width > max_width) max_width = width;
+    }
 
     for (list.items) |i| {
-        try stdout.print("{s}\n", .{i.name}); 
+        const name = std.c.getpwuid(i.uid);
+        const group = std.c.getgrgid(i.gid);
+
+        try stdout.print("\x1b[32m{[user]s} \x1b[33m{[group]s} \x1b[34m{[size]d:>[width]} \x1b[35m{[name]s:<5}\n", .{
+            .user = name.?.name.?, 
+            .group = group.?.name.?, 
+            .size = i.size, 
+            .width = max_width, 
+            .name = i.name
+        }); 
     }
 
     filestat.deinit(&list);
