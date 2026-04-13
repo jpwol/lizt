f794c96085fb7e6fc232d022e6329cd0ae46ee4d
jpwol <jpwolfe1981@gmail.com>
2026-04-13 03:03:07 -0400

    feat(file): added perm string builder

--- src/file.zig
+++ src/file.zig
@@ -8,6 +8,16 @@ const linux = std.os.linux;
 const posix = std.posix;
 const Allocator = std.mem.Allocator;
 
+const RDUSR = 0o400;
+const WRUSR = 0o200;
+const EXUSR = 0o100;
+const RDGRP = 0o040;
+const WRGRP = 0o020;
+const EXGRP = 0o010;
+const RDOTH = 0o004;
+const WROTH = 0o002;
+const EXOTH = 0o001;
+
 pub const FileStats = struct {
     name: []const u8,
     kind: File.Kind,
@@ -16,6 +26,7 @@ pub const FileStats = struct {
     uid: linux.uid_t,
     gid: linux.gid_t,
     nlink: u32,
+    perm: [10]u8
 };
 
 const Statx = struct {
@@ -57,6 +68,32 @@ pub fn getstatx(path: [*:0]const u8) ?Statx {
     }
 }
 
+fn buildPermString(kind: File.Kind, mode: u16) [10]u8 {
+    var buf: [10]u8 = undefined; 
+    buf[0] = switch(kind) {
+        .directory => 'd',
+        .sym_link => 'l',
+        .block_device => 'b',
+        .character_device => 'c',
+        .unix_domain_socket => 's',
+        .named_pipe => 'p',
+        .file => '-',
+        else => '?',
+    };
+
+    buf[1] = if (mode & RDUSR != 0) 'r' else '-';
+    buf[2] = if (mode & WRUSR != 0) 'w' else '-';
+    buf[3] = if (mode & EXUSR != 0) 'x' else '-';
+    buf[4] = if (mode & RDGRP != 0) 'r' else '-';
+    buf[5] = if (mode & WRGRP != 0) 'w' else '-';
+    buf[6] = if (mode & EXGRP != 0) 'x' else '-';
+    buf[7] = if (mode & RDOTH != 0) 'r' else '-';
+    buf[8] = if (mode & WROTH != 0) 'w' else '-';
+    buf[9] = if (mode & EXOTH != 0) 'x' else '-';
+
+    return buf;
+}
+
 pub fn buildDirList(self: Self) !std.ArrayList(FileStats) {
     const d = try Dir.cwd().openDir(self.io, self.path, .{ .follow_symlinks = false, .iterate = true });
     var itr = d.iterate();
@@ -77,6 +114,7 @@ pub fn buildDirList(self: Self) !std.ArrayList(FileStats) {
                 .nlink = s.nlink,
                 .uid = s.uid,
                 .gid = s.gid,
+                .perm = buildPermString(i.kind, s.mode),
             });
         }
     }
