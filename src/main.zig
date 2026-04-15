434c937084fdb293a4e00dc34d006ebe4f0b2e02
jpwol <jpwolfe1981@gmail.com>
2026-04-15 12:27:47 -0400

    refactor: using juicy main

--- src/main.zig
+++ src/main.zig
@@ -3,41 +3,29 @@ const file = @import("file.zig");
 const linux = std.os.linux;
 const posix = std.posix;
 
-var threaded: std.Io.Threaded = undefined;
-
-pub fn main(init: std.process.Init.Minimal) !void {
-    const c_allocator = std.heap.c_allocator;
-    var arena_impl = std.heap.ArenaAllocator.init(c_allocator);
-    defer arena_impl.deinit();
-    const allocator = arena_impl.allocator();
-
-    threaded = std.Io.Threaded.init(c_allocator, .{
-    });
-    defer threaded.deinit();
-    const io = threaded.io();
-    
-    const argv = try init.args.toSlice(allocator);
-    defer allocator.free(argv);
+pub fn main(init: std.process.Init) !void {
+    const allocator = init.arena.allocator();
+    const argv = try init.minimal.args.toSlice(allocator);
 
     const stdout = blk: {
         var buf: [4096]u8 = undefined;
-        var stdout = std.Io.File.stdout().writer(io, &buf);
+        var stdout = std.Io.File.stdout().writer(init.io, &buf);
         break :blk &stdout.interface;
     };
     const stderr = blk: {
-        var stderr = std.Io.File.stderr().writer(io, &.{});
+        var stderr = std.Io.File.stderr().writer(init.io, &.{});
         break :blk &stderr.interface;
     };
 
     const path = if (argv.len == 2) argv[1] else ".";
 
     const cwd = std.Io.Dir.cwd();
-    const s = cwd.statFile(io, path, .{ .follow_symlinks = false }) catch |err| {
+    const s = cwd.statFile(init.io, path, .{ .follow_symlinks = false }) catch |err| {
         try stderr.print("{s}: '{s}': {s}\n", .{argv[0], path, @errorName(err)});
         return;
     };
 
-    var filestat = file.init(path, s.kind, io, allocator);
+    var filestat = file.init(path, s.kind, init.io, allocator);
 
     filestat.printlist(stdout) catch |err| {
         try stderr.print("{s}: '{s}': {s}\n", .{argv[0], path, @errorName(err)});
